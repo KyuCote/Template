0efdaa24f65f44eb8b2fe6e6cff5271f8f879db9
KyuCote <143118321+KyuCote@users.noreply.github.com>
2024-02-06 09:38:16 -0500

    script

--- Assets/Scripts/Nuages.cs
+++ Assets/Scripts/Nuages.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class nUAGES : MonoBehaviour
+public class Nuages : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float _positionX;
+    private float _positionY;
+    private float _positionZ;
+    [SerializeField] private float _vitesse;
+
     void Start()
     {
-        
+        _positionX = transform.position.x;
+        _positionY = transform.position.y;
+        _positionZ = transform.position.z; 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        BougeNuage();
+    }
+
+    void BougeNuage(){
         
+        //------------------------------------------------------- Sert à changer la position x des nuages
+
+        // transform.position = new Vector3(_positionX += (_vitesse*Time.deltaTime), _positionY, _positionZ);
+
+        //------------------------------------------------------- Sert à avancer le nuage
+
+        transform.Translate(Vector3.forward *_vitesse *Time.deltaTime);
+        transform.Translate(Vector3.up *_vitesse *Time.deltaTime);
+
     }
 }
