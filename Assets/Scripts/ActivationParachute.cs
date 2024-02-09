cc4351a38eb2b9c0c72ba9768f53753e486a358d
KyuCote <143118321+KyuCote@users.noreply.github.com>
2024-02-09 11:43:30 -0500

    meow

---
+++ Assets/Scripts/ActivationParachute.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationParachute : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private GameObject _zoneDanger;
+    [SerializeField] private GameObject _parachute;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void OnTriggerEnter(Collider other){
+        
+        Debug.Log(other.gameObject.tag);
+
+        if(other.gameObject.tag == "ZoneMort"){
+
+        }
+    }
+}
